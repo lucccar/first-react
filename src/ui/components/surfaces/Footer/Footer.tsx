e5be26024c6eae10951fc561e9b71e99580fa4d5
Lucas Carvalho <lucas.crvlh.s@gmail.com>
2021-06-13 19:06:15 -0300

    adding footer comopnent and styling

--- src/ui/components/surfaces/Footer/Footer.tsx
+++ src/ui/components/surfaces/Footer/Footer.tsx
@@ -1,20 +1,40 @@
 import React from "react";
-import { FooterStyled, FooterContainer, FooterTitle } from "./Footer.style";
-import { Typography } from "@material-ui/core";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
 
 const Footer = () => {
   return (
     <FooterStyled>
       <FooterContainer>
-        footer
-        <FooterTitle> Quem Somos </FooterTitle>
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Reprehenderit, similique sapiente deserunt iure esse vero doloremque
-          at rerum est laudantium, iusto delectus hic, excepturi dignissimos eos
-          reiciendis? A, quo dolores?
-        </Typography>
-        <FooterTitle> Baixe nossos aplicativos. </FooterTitle>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle> Quem Somos </FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit, similique sapiente deserunt iure esse vero doloremque
+            at rerum est laudantium, iusto delectus hic, excepturi dignissimos
+            eos reiciendis? A, quo dolores?
+          </Typography>
+        </Box>
+        <div>
+          <FooterTitle> Baixe nossos aplicativos. </FooterTitle>
+          <AppList>
+            <li>
+              <a href="/" target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt="app store" />
+              </a>
+            </li>
+            <li>
+              <a href="/" target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt="google play" />
+              </a>
+            </li>
+          </AppList>
+        </div>
       </FooterContainer>
     </FooterStyled>
   );
